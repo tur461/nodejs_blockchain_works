44d6ae0d5dd6be16322791ec967354a35c8f656d
tur461 <60494822+tur461@users.noreply.github.com>
2021-10-23 17:22:13 +0530

    ...

--- web3/basic_eth_methods.js
+++ web3/basic_eth_methods.js
@@ -1,8 +1,10 @@
 const Web3 = require('web3');
+require('dotenv').config();
 
 console.log('working..');
+const token = process.env.TOKEN;
 
-const url = 'https://mainnet.infura.io/v3/3a25827bef8e449d85750255d6521b3d';
+const url = `https://mainnet.infura.io/v3/${token}`;
 
 const web3 =  new Web3(url);
 
